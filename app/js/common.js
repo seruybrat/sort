9463b277b2105f00fb10a7992e52c10ef49c95f5
sergii.bartkiv <Sergii.Bartkiv@softengi.com>
2017-11-10 18:51:31 +0200

    add sort func

--- app/js/common.js
+++ app/js/common.js
@@ -10,13 +10,13 @@
             account: 123456000
         },
         {
-            order: 2,
+            order: 3,
             flag: 'spa',
             sum: 1111,
             account: 111456000
         },
         {
-            order: 3,
+            order: 2,
             flag: 'chi',
             sum: 2221,
             account: 223456000
@@ -41,9 +41,18 @@
         }
     ];
 
-    function drawArr() {
+    var e = e || window.event;
+    var sortUpArr = document.querySelectorAll('.sort-up');
+    var sortDownArr = document.querySelectorAll('.sort-down');
 
-        var target = document.getElementById('tournament-list');
+    if (localStorage.getItem("itemsArr") === null) {
+        localStorage.setItem("itemsArr", JSON.stringify(itemsArr));
+    } else {
+        itemsArr = JSON.parse(localStorage.getItem("itemsArr"));
+    }
+
+
+    function drawArr(target) {
 
         for (var i = 0; i < itemsArr.length; i++) { 
 
@@ -61,33 +70,87 @@
                     '</span>' +
                      '<span class="cell-account">' + itemsArr[i].account +
                     '</span>' +
-                    '<span class="cell-country flag-' + itemsArr[i].flag + 
-                    '"></span>' +
+                    '<span class="cell-country">' + itemsArr[i].flag + 
+                    '</span>' +
                 '</div>' +
 
             '</li>';
 
-            target.innerHTML += item;
+            document.getElementById(target).innerHTML += item;
+
+        }
+    }
+
+    function clearArr(target) {
+        document.getElementById(target).innerHTML = "";
+    }
+
+    function sortBy(field, reverse) {
+
+        var key = function(x) {
+            return x[field];
+        };
+
+        reverse = !reverse ? 1 : -1;
+
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+        };
+        
+    }
+
+    function redrawArr(e, reverse) {
+        switch(e.target.parentNode.className) {
+
+            case 'cell-number': 
+                itemsArr.sort(sortBy('order', reverse));
+                break;
+
+            case 'cell-sum': 
+                itemsArr.sort(sortBy('sum', reverse));
+                break;
+
+            case 'cell-account': 
+                itemsArr.sort(sortBy('account', reverse));
+                break;
+
+            case 'cell-country': 
+                itemsArr.sort(sortBy('flag', reverse));
+                break;
+
+        }
+
+        clearArr('tournament-list');
+        drawArr('tournament-list');
+        localStorage.setItem("itemsArr", JSON.stringify(itemsArr));
+       
+    }
+
+    function getUpDownArrows() {
+
+        for (var i = 0; i < sortUpArr.length; i++) {
+
+            sortUpArr[i].addEventListener('click', function(e) {
+                redrawArr(e, false);
+
+            });
+
+        }
+
+        for (var j = 0; j < sortDownArr.length; j++) {
+
+            sortDownArr[j].addEventListener('click', function(e) {
+                redrawArr(e, true);
+            });
 
         }
     }
 
     document.addEventListener("DOMContentLoaded", function() {
 
-        var numUp = document.getElementById('num');
+        drawArr('tournament-list');
+        getUpDownArrows();
 
-        numUp.addEventListener('click', function() {
-
-            var numArray = document.querySelectorAll('.cell-number');
-            numArray[0].style.backgroundColor = "red";
-
-            function sortNumber(a,b) {
-                return a - b;
-            }
-        });
-
-
-        drawArr();
 
     });
 
